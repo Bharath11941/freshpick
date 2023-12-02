35d26424c15092f1a8503cf2755e5abdef3a531f
Bharath <bharathpalakkal@gmail.com>
2023-12-02 13:22:12 +0530

    before hosting

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 const noCache = require("./middlewares/cache");
 const cartCount = require("./middlewares/cartCount");
 const wishlistCount = require("./middlewares/wishlistCount");
